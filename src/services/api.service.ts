38b839537491866815ebe45fc49185b2955516bb
AlexJohannson <mehedsanya@gmail.com>
2025-01-08 20:19:09 +0100

    lesson-3/task/2

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -5,13 +5,13 @@ const dummyUrl = import.meta.env.VITE_DUMMY_API_URL;
 
 export const dummyUserService = {
     getAllUsers: async (): Promise<IUserResponsModel> => {
-        return await fetch(dummyUrl + '/users')
+        return await fetch(`${dummyUrl}/users`)
             .then(value => value.json());
     }
 };
 export const dummyCartService = {
     getCartsOfUsers: async(userId: string): Promise<IResponsProduct> => {
-        return await fetch(dummyUrl + '/carts/user/' + userId)
+        return await fetch(`${dummyUrl}/carts/user/${userId}`)
             .then(res => res.json());
     }
 };
